769c703c0db8f4647d0b377caffb1b86bdb0bfbd
Hector <hdavidfajardo109@gmail.com>
2023-06-08 15:32:55 -0500

    se agrega galeria de imagenes

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let carrusel = document.getElementById("carrusel");
+let imagenes = [
+  "../imagenes/Fotos/img1.webp",
+  "../imagenes/Fotos/img2.webp",
+  "../imagenes/Fotos/img3.webp",
+  "../imagenes/Fotos/img4.webp",
+  "../imagenes/Fotos/img5.webp",
+  "../imagenes/Fotos/img6.webp",
+  "../imagenes/Fotos/img7.webp",
+  "../imagenes/Fotos/img8.webp",
+  "../imagenes/Fotos/img9.webp",
+  "../imagenes/Fotos/img10.webp",
+  "../imagenes/Fotos/img11.webp",
+  "../imagenes/Fotos/img12.webp"
+];
+
+for (let i = 0; i < 20; i++) {
+  let spanElement = document.createElement("span");
+  let randomIndex = Math.floor(Math.random() * imagenes.length);
+  let randomImage = imagenes[randomIndex];
+  spanElement.style.backgroundImage = `url(${randomImage})`;
+  carrusel.appendChild(spanElement);
+}
