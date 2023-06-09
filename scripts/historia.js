cd13b9b2c6593d17b64b838f40ed980c0a931836
Hector <hdavidfajardo109@gmail.com>
2023-06-09 11:51:12 -0500

    agrego interacción

--- scripts/historia.js
+++ scripts/historia.js
@@ -1,32 +1,40 @@
-// Seleccionar todos los elementos <article>
-  const articles = document.querySelectorAll('article');
-
-// Valor inicial de inset
+const articles = document.querySelectorAll("article");
 let insetValue = 0;
+let h2 = document.getElementById("h2");
+let instructor = document.getElementById("instructor");
+let historia1 = document.getElementById("his1");
+let historia2 = document.getElementById("his2");
+let article = document.querySelectorAll("article");
 
-// Iterar sobre cada elemento <article> en orden ascendente
+//función para agregar propiedad inset a los articulos
+
 for (let i = 0; i < articles.length; i++) {
-  // Incrementar el valor de inset
   insetValue += 70;
-
-  // Aplicar el nuevo valor de inset al elemento
   articles[i].style.top = `${insetValue}px`;
 }
 
-let h2 = document.getElementById("h2");
-let instructor = document.getElementById("instructor")
-let historia1 = document.getElementById("his1")
+//Función de cambio en los elementos e historias
 
-function centro(){
-  let f1 = document.getElementById("f1");
-  f1.style.background = "var(--main-bg-color-1)"
-  f1.style.transform = "scale(110%)"
-  f1.style.width = "400px"
-  f1.style.borderRadius = "0 20px 100px 0"
-  f1.style.alignItems = "center"
-  h2.style.marginBottom = "0"
-  h2.style.color = "var(--main-bg-color)"
-  instructor.style.display = "none"
-  historia1.style.display = "flex"
-}
-
+article.forEach((element) => {
+  element.onclick = centro;
+  function centro() {
+    if (element.id === "f1") {
+      let f1 = document.getElementById("f1");
+      f1.style.background = "var(--main-bg-color-1)";
+      f1.style.transform = "scale(110%)";
+      f1.style.width = "400px";
+      f1.style.borderRadius = "0 20px 100px 0";
+      f1.style.alignItems = "center";
+      h2.style.marginBottom = "0";
+      h2.style.color = "var(--main-bg-color)";
+      instructor.style.display = "none";
+      historia1.style.display = "flex";
+      historia2.style.display = "none";
+    } else if (element.id === "f2") {
+      historia2.style.display = "flex";
+      instructor.style.display = "none";
+      historia1.style.display = "none";
+    }
+  }
+  console.log(element);
+});
