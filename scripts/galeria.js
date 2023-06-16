c3a8f057e25e450bd43293499b5baded9acfd6a8
Hector <hdavidfajardo109@gmail.com>
2023-06-16 10:57:42 -0500

    se cambian algunos estilos

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -1,35 +1,67 @@
-let selectores = document.querySelector('.selectores')
-let selects = document.querySelector('.select')
-let sel = document.querySelectorAll('button')
-let desplazamiento = 0;
-let izquierda = document.getElementById('left')
-let derecha = document.getElementById('right')
-derecha.onclick = dere
-izquierda.onclick = izq
+var imagenes = document.querySelectorAll('.gallery img');
+var currentIndex = 0;
 
-function limpiar() {
-  sel.forEach((element, i) => {
-    element.style.transform = "scale(1)"
+function mostrarImagenFullscreen(index) {
+  var imagen = imagenes[index];
+
+  if (imagen.requestFullscreen) {
+    imagen.requestFullscreen();
+  } else if (imagen.mozRequestFullScreen) {
+    imagen.mozRequestFullScreen();
+  } else if (imagen.webkitRequestFullscreen) {
+    imagen.webkitRequestFullscreen();
+  } else if (imagen.msRequestFullscreen) {
+    imagen.msRequestFullscreen();
+  }
+}
+
+function cambiarImagen(direccion) {
+  if (direccion === 'anterior') {
+    currentIndex = (currentIndex - 1 + imagenes.length) % imagenes.length;
+  } else if (direccion === 'siguiente') {
+    currentIndex = (currentIndex + 1) % imagenes.length;
+  }
+
+  mostrarImagenFullscreen(currentIndex);
+}
+
+document.addEventListener('keydown', function(event) {
+  if (document.fullscreenElement && (event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
+    event.preventDefault();
+
+    if (event.key === 'ArrowLeft') {
+      cambiarImagen('anterior');
+    } else if (event.key === 'ArrowRight') {
+      cambiarImagen('siguiente');
+    }
+  }
+});
+
+var arrowLeft = document.querySelector('.arrow-left');
+var arrowRight = document.querySelector('.arrow-right');
+
+arrowLeft.addEventListener('click', function() {
+  cambiarImagen('anterior');
+});
+
+arrowRight.addEventListener('click', function() {
+  cambiarImagen('siguiente');
+});
+
+for (var i = 0; i < imagenes.length; i++) {
+  imagenes[i].addEventListener('click', function() {
+    var index = Array.from(imagenes).indexOf(this);
+    currentIndex = index;
+    mostrarImagenFullscreen(index);
   });
 }
 
-function desplazar(event) {
-  limpiar();
-  
-  function dere(){
-    desplazamiento -= 300;
+document.addEventListener('fullscreenchange', function() {
+  if (document.fullscreenElement) {
+    arrowLeft.style.display = 'block';
+    arrowRight.style.display = 'block';
+  } else {
+    arrowLeft.style.display = 'none';
+    arrowRight.style.display = 'none';
   }
-function izq(){
-    desplazamiento += 300;
-  }
-  
-  selects.style.transform = `translateX(${desplazamiento}px)`
-  
-  sel.forEach((element, i) => {
-    element.addEventListener('click', ampliar)
-  });
-}
-
-
-console.log(selects);
-console.log(selectores);
+});
