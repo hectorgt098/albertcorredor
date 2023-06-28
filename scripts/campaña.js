b575c4925dd2a387e4b59a71275c4f2cefe273b7
Hector <hdavidfajardo109@gmail.com>
2023-06-28 15:10:49 -0500

    a

--- scripts/campaña.js
+++ scripts/campaña.js
@@ -4,5 +4,5 @@ let elem = Array.from(t1)
 elem.forEach(function(element, index) {
   setTimeout(function() {
     element.style.animation = "spaceInRight 1s ease forwards";
-  }, (index * 500) + 1000); // 0.5 segundos de retraso progresivo después de 2 segundos
+  }, (index * 500) + 1000);
 });
